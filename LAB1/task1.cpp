b2ad1dc9b2745a68178bc3f930bd2a6b43eebc45
6ogdashka <bahdan.krasnenka@gmail.com>
2025-11-17 09:25:33 +0300

    LAB 0.0

--- LAB1/task1.cpp
+++ LAB1/task1.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
 
+double get_double_value ();
+
 int main() {
+    try {
     double a;
     std::cout << "Введите сторону квадрата a: ";
-    std::cin >> a;
+    a = get_double_value();
 
     double perimeter = 4 * a;
     double area = a * a;
 
     std::cout << "Периметр: " << perimeter << "\n";
     std::cout << "Площадь: " << area << "\n";
-
+    } catch ( const char* msg) { std::cout << msg;}
     return 0;
+}
+
+double get_double_value () {
+    double value;
+    if (( std::cin >> value ) && ( value > 0)) {
+        return value;
+    } else {
+        throw " Число должно быть натуральным ";
+    }
 }
