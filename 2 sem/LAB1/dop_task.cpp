89bdb6261c164b133c384d16b41d21bcf860b003
6ogdashka <bahdan.krasnenka@gmail.com>
2026-02-14 23:14:42 +0300

    00

--- 2 sem/LAB1/dop_task.cpp
+++ 2 sem/LAB1/dop_task.cpp
@@ -1,47 +1,52 @@
 #include <iostream>
-#include <math.h>
-
+#include <cmath>
+#include <iomanip>
 
 int main() {
-
     double x;
     std::cout << "Enter x: ";
     std::cin >> x;
 
-    double Y_cpp = std::pow((double)std::pow(std::abs(atan(x)),2.0*cos(x)),2.0);
+    long double base = std::fabsl(std::atanl((long double)x));
+    long double exponent = 4.0L * std::cosl((long double)x);
+    double Y_cpp = (double)std::pow(base, exponent);
     double Y_asm{};
 
     __asm__ volatile (
-        "finit\n"
-        "fld QWORD PTR %[x]\n"
-        "fld1\n"
-        "fpatan\n"
-        "fabs\n"
-        "fld QWORD PTR %[x]\n"
-        "fcos\n"
-        "fxch ST(1)\n"
-        "fyl2x\n"
-        "fadd ST(0)\n"
-        "fld1\n"
-        "fscale\n"
-        "fxch ST(1)\n"
-        "fld ST(0)\n"
-        "frndint\n"
-        "fsub ST(1),ST(0)\n"
-        "fstp ST(0)\n"
-        "f2xm1\n"
-        "fld1\n"
-        "fadd ST(1),ST(0)\n"
-        "fstp ST(0)\n"
-        "fmul ST(0),ST(1)\n"
-        "fstp ST(1)\n"
-        "fmul ST(0)\n"
-        "fstp QWORD PTR %[Y]\n"
-        : [Y] "=m" (Y_asm)
-        : [x] "m" (x)
-        : "st"
+        
+    "finit\n"
+    "fld QWORD PTR %[x]\n"
+    "fld1\n"
+    "fpatan\n"
+    "fabs\n"
+
+    "fld QWORD PTR %[x]\n"
+    "fcos\n"
+    "fadd ST(0), ST(0)\n"
+    "fxch ST(1)\n"
+    "fyl2x\n"
+
+    "fld ST(0)\n" // продублировал степень
+    "frndint\n" // получил целую часть
+
+    "fsub ST(1), ST(0)\n" // теперь в st1 находится дробная часть, а в st0 целая
+    "fxch ST(1)\n"
+    "f2xm1\n"
+    "fld1\n"
+    "faddp ST(1), ST(0)\n" // получили 2 в степени f, а в st(1) будет лежать целая часть
+
+    "fscale\n"
+    "fmul ST(0), ST(0)\n"
+    "fstp QWORD PTR %[Y]\n"
+    "fstp ST(0)\n" // очистка мусора
+    : [Y] "=m" (Y_asm)
+    : [x] "m" (x)
+    : "st" 
     );
     
-    std::cout << "C++ result:" << Y_cpp << "\n";
-    std::cout << "ASM result:" << Y_asm << "\n";
+    std::cout << std::fixed << std::setprecision(15);
+    std::cout << "C++ result: " << (double)Y_cpp << "\n";
+    std::cout << "ASM result: " << Y_asm << "\n";
+
+    return 0;
 }
